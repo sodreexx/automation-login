d3dbc4f16940f0f579fe05d4cdc887fe6e8f96df
sodree <lucasssodre2003@gmail.com>
2024-11-25 23:35:10 -0300

    first commit

---
+++ cypress/e2e/loginspec.cy.js
@@ -0,0 +1,19 @@
+describe('orenge', () => {
+  it('login - success', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type('Admin')
+    cy.get("[name='password']").type('admin123')
+    cy.get('.oxd-button').click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index' )
+    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard')
+  })
+  
+  it('login - fail', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type('test')
+    cy.get("[name='password']").type('test')
+    cy.get('.oxd-button').click()
+    cy.get('.oxd-alert')
+   
+  })
+})
